92e59f09cb406ff4f364555f2ab6ecde873f3474
lyr <2402077254@qq.com>
2025-01-18 19:58:33 +0800

    branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is a branc");
-        System.out.println("Hell world!");
         System.out.println("this is a branch");
+
+
+        System.out.println("Hell world!");
     }
 }
