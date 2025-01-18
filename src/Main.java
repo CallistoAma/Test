7784bdfe0d7d8398b4bc583feceb96c107bd4df9
lyr <2402077254@qq.com>
2025-01-18 20:13:37 +0800

    branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is a ");
+        System.out.println("");
         System.out.println("Hell world!");
         System.out.println("");
     }
