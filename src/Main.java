75e537513a884d14d91d2dcf99cfb30334ba7dbe
lyr <2402077254@qq.com>
2025-01-18 20:18:35 +0800

    master

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("master");
 
 
         System.out.println("");
-        System.out.println("Hell world!");
+        System.out.println("");
     }
 }
